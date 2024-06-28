e1fb4b172c1193422030de0346a178ba11675c22
maheshryali1122 <159117395+maheshryali1122@users.noreply.github.com>
2024-06-28 15:22:04 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   type = string
   default = "us-west-2"
 }
-variable "docker_tag" {
-  type = string
-  default = "1.0"
-}
 variable "user" {
   type = string
 }
@@ -13,19 +9,15 @@ variable "privatekey" {
   type        = string
   sensitive   = true
 }
-variable "instancetype" {
+variable "instance_type" {
   type = string
-}
-variable "docker_username" {
-  type = string
-}
-variable "docker_password" {
-  type = string
-  sensitive = true
 }
 variable "number" {
   type = string
   default = "1.0"
+}
+variable "key_name" {
+  type =string
 }
 
 
