c776970e707b35f6a206c9bcef78bd125961eabc
maheshryali1122 <159117395+maheshryali1122@users.noreply.github.com>
2024-06-28 15:30:56 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   type = string
   default = "us-west-2"
 }
-variable "privatekey" {
-  type        = string
-  sensitive   = true
-}
 variable "instance_type" {
   type = string
 }
